cc21529bbb687a15033c657eed9fac94e176f7d5
Abraão Alves da Silva <abraaocursos2019@gmail.com>
2020-11-14 21:54:13 -0300

    Programa_005
    Converso sistema métrico decimal.

---
+++ Estrutura Sequêncial/Convertendo para centímetro.py
@@ -0,0 +1,13 @@
+"""
+Programa 005
+Área de estudos.
+data 14.11.2020     (Indefinida) Hs
+@Autor: Abraão A. Silva
+"""
+# Coleta de dado pelo teclado.
+metros = float(input('Valor(m).: '))
+
+# Calculo de conversão.
+centimetros = (metros * 100)
+# Saída do Programa.
+print(f'{metros} metros equivale a {centimetros:.2f} centímetros.')
