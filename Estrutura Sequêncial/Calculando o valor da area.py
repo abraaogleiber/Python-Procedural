b5a2b20b2abdd94eb33906d5524701692b64387a
Abraão Alves da Silva <abraaocursos2019@gmail.com>
2020-11-15 06:16:16 -0300

    Programa_006
    Calculo de área de uma circunferência.

---
+++ Estrutura Sequêncial/Calculando o valor da area.py
@@ -0,0 +1,13 @@
+"""
+Programa 006
+Área de estudos.
+data 15.11.2020     (Indefinida) Hs
+@Autor: Abraão A. Silva
+"""
+
+# Calculo da área de uma circunferência.
+# Formula A = Pi.r**2
+
+raio = float(input('Valor do Raio.: '))
+area = 3.14 * raio**2
+print(f'A circunferência, cujo raio é {raio:.1f} tem área igual a {area:.1f}.')
