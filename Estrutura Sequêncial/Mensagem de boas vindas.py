0569ab94f07c7b6aea50c99ef6adf4b606bd6738
Abraão Alves da Silva <abraaocursos2019@gmail.com>
2020-11-14 21:42:02 -0300

    Programa_001 (Atualizado)
    Coloquei um cabeçalho no meu script para identificação.

--- Estrutura Sequêncial/Mensagem de boas vindas.py
+++ Estrutura Sequêncial/Mensagem de boas vindas.py
@@ -1,1 +1,9 @@
+"""
+Programa 001
+Área de estudos.
+data 14.11.2020     (Indefinida) Hs
+@Autor: Abraão A. Silva
+"""
+
+# Saída de string, utilizando o "print".
 print('Olá, Mundo; Dei-me as boas vindas.')
