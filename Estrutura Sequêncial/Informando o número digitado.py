7c306304abcb0af1539aaa8d9bd5f7a549669a28
Abraão Alves da Silva <abraaocursos2019@gmail.com>
2020-11-14 21:43:48 -0300

    Programa_002
    Saída de dados.

---
+++ Estrutura Sequêncial/Informando o número digitado.py
@@ -0,0 +1,23 @@
+"""
+Programa 002
+Área de estudos.
+data 14.11.2020     (Indefinida) Hs
+@Autor: Abraão A. Silva
+"""
+
+
+# Modo 1
+numero = int(input('Informe um número.: '))
+print('O número informado foi {}'.format(numero))
+
+# Modo 2
+numero = int(input('Informe um número.: '))
+print(f'O número informado foi {numero}')
+
+# Modo 3
+numero = int(input('Informe um número.: '))
+print('O número informado foi ' + str(numero))  # Antigo modo de formatação.
+
+# Modo 4
+numero = int(input('Informe um número.: '))     # Antigo modo de formatação
+print('O número informado foi', numero)
