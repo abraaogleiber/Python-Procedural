c04dcb66d3101856e24276deda3c88cc18f92c88
Abraão Alves da Silva <abraaocursos2019@gmail.com>
2020-11-17 21:46:14 -0300

    Programa_019
    Negativo ou positivo.

---
+++ Estrutura Condicional/Negativo ou positivo.py
@@ -0,0 +1,15 @@
+"""
+Programa 019
+Área de estudos.
+data 18.11.2020     (Indefinida) Hs
+@Autor: Abraão A. Silva
+"""
+
+# Entrada do usuário com um valor.
+numero = int(input('Digite um valor numerico.: '))
+
+# Verificação. Onde "numero" é maior que "zero" ou menor. Condição composta ou binária.
+if numero < 0:
+    print(f'O numero {numero} é Negativo.')
+else:
+    print(f'O numero {numero} é Positivo.')
