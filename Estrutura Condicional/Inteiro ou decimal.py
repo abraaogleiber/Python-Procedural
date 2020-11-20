91984952a290f7b17198602ba549e10a301c836d
Abraão Alves da Silva <abraaocursos2019@gmail.com>
2020-11-20 20:42:30 -0300

    Programa_041
    DEcimal ou inteiro.

---
+++ Estrutura Condicional/Inteiro ou decimal.py
@@ -0,0 +1,14 @@
+"""
+Programa 041
+Área de estudos.
+data 20.11.2020     (Indefinida) Hs
+@Autor: Abraão A. Silva
+"""
+
+# Verificando se um número é dicimal ou inteiro.
+numero = input('Digite um número.: ')
+
+if '.' in numero:
+    print('Decimal')
+else:
+    print('Inteiro')
