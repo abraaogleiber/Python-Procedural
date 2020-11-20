4e69073215ff0af628189dd134fc22cdaf2ad3a0
Abraão Alves da Silva <abraaocursos2019@gmail.com>
2020-11-20 20:29:56 -0300

    Programa_040
    Par ou impar.

---
+++ Estrutura Condicional/Par ou impar.py
@@ -0,0 +1,15 @@
+"""
+Programa 040
+Área de estudos.
+data 20.11.2020     (Indefinida) Hs
+@Autor: Abraão A. Silva
+"""
+
+# Entrada pelo usuário.
+num = int(input('Digite um número.: '))
+
+# Condição binária, verifica se número é par ou impar. 
+if num % 2 == 0:
+    print(f'O número {num} é "PAR".')
+else:
+    print(f'O número {num} NÃO É PAR.')
