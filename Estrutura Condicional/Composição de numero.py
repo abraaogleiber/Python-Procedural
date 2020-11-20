8d86d474fd0a9b85123e22772e64fb2da48dcce0
Abraão Alves da Silva <abraaocursos2019@gmail.com>
2020-11-19 21:26:00 -0300

    Programa_037
    Decompondo um número.

---
+++ Estrutura Condicional/Composição de numero.py
@@ -0,0 +1,22 @@
+"""
+Programa 037
+Área de estudos.
+data 20.11.2020     (Indefinida) Hs
+@Autor: Abraão A. Silva
+"""
+
+numero = int(input('Digite um número (menor que mil).: '))
+
+if numero < 1000:
+    centena = int((numero % 1000) / 100)
+    dezena = int((numero % 100) / 10)
+    unidade = numero % 10
+
+    if unidade == 0 and dezena != 0 and centena != 0:
+        print(f'O valor é composto por {centena} Centena(s) e {dezena} Dezena(s).')
+    elif unidade == 0 and dezena == 0 and centena != 0:
+        print(f'O valor é composto por {centena} Centena(s).')
+    elif unidade != 0 and dezena == 0 and centena != 0:
+        print(f'O valor é composto por {centena} Centena(s) e {unidade} Unidade(s).')
+    elif unidade != 0 and dezena != 0 and centena != 0:
+        print(f'O valor é composto por {centena} Centena(s), {dezena} Dezena(s) e {unidade} Unidade(s).')
