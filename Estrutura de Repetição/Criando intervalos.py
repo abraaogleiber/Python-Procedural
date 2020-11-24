f2918f2baf40b340603c4efee9fa239dc82a1eac
Abraão Alves da Silva <abraaocursos2019@gmail.com>
2020-11-24 09:03:24 -0300

    Programa_055 (Atualizado)
    Gerando valores em um intervalo e somandos.

--- Estrutura de Repetição/Criando intervalos.py
+++ Estrutura de Repetição/Criando intervalos.py
@@ -10,11 +10,19 @@ n1 = int(input('Digite um número.: '))
 n2 = int(input('Digite outro número.: '))
 
 # Geramos os valores do intervalo passado (N1,N2).
+# Atualizado; faz o calculo dos valores no intervalo.
+soma = int()
 if n1 < n2:
     for n in range(n1+1, n2):
         print(n, end=' ')
+        soma += n
 elif n2 < n1:
     for n in range(n2+1, n1):
         print(n, end=' ')
+        soma += n
 else:
     print('Não computamos valores iguais.')
+    exit()
+
+
+print('\n'+f'A soma dos valores valem {soma}.')
