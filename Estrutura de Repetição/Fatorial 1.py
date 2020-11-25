8ff1c384395578a1d257065cc26b2a1bdea088bc
Abraão Alves da Silva <abraaocursos2019@gmail.com>
2020-11-25 10:23:35 -0300

    Programa_061 (Atualizado)
    Fatorial de um número. Nesta atualização permitimos ao usuário repetir n-vezes o mesmo processo.

--- Estrutura de Repetição/Fatorial 1.py
+++ Estrutura de Repetição/Fatorial 1.py
@@ -5,20 +5,31 @@ data 25.11.2020     (Indefinida) Hs
 @Autor: Abraão A. Silva
 """
 
-# Entrada do valor.
-print('Descubra o fatorial de um número')
-valor = int(input('Número.: '))
+while True:
+    # Entrada do valor.
+    print('Descubra o fatorial de um número')
+    valor = int(input('Número.: '))
 
-# O processo encontra o fatorial do valor.
-fatorial = 1
-print(f'{valor}!', end='= ')
-while valor > 0:
-    if valor != 1: # Condição como método de formatação.
-        print(valor, end='x')
-    else:
-        print(valor, end=' = ')
-    
-    fatorial *= valor # Calculo do fatorial.
-    valor -= 1 # Processo de descremento até que chegemos ao caso base.
+    # O processo encontra o fatorial do valor.
+    fatorial = 1
+    print(f'{valor}!', end='= ')
+    while valor > 0:
+        if valor != 1: # Condição como método de formatação.
+            print(valor, end='x')
+        else:
+            print(valor, end=' = ')
+        
+        fatorial *= valor # Calculo do fatorial.
+        valor -= 1 # Processo de descremento até que cheguemos ao caso base.
 
-print(fatorial)
+    print(fatorial)
+
+    # Perguntamos ao usuário se ele deseja continuar o programa.
+    pergunta = str(input('Desejas continuar(S/N)?.: ')).strip().upper()[0]
+    while pergunta not in 'SN':
+        print('Resposta inválida..')
+        pergunta = str(input('Desejas continuar(S/N)?.: ')).strip().upper()[0]
+    if pergunta == 'S':
+        continue
+    elif pergunta == 'N':
+        break
