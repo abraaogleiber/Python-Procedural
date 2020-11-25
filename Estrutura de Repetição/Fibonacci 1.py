e63a7e72c4283d028a220578d4d3dd33c420c69e
Abraão Alves da Silva <abraaocursos2019@gmail.com>
2020-11-25 08:52:31 -0300

    Programa_059 (Atualizado)
    Sequência de fibonacci, versão 1

--- Estrutura de Repetição/Fibonacci 1.py
+++ Estrutura de Repetição/Fibonacci 1.py
@@ -9,14 +9,14 @@ data 25.11.2020     (Indefinida) Hs
 quant_termo = int(input('Quantos termos desejas mostrar.: ')) + 1
 
 # Definimos o primeiro e segundo termo.
-t1 = 1
-t2 = 1
+t1 = t2 = 1 # Apelidagem de valores. (t1 e t2 são referências ao mesmo objeto.)
 
 # Saída dos dados e geração do termo posterior, somando os dois anteriores.
-print(t1, end=' ')
-print(t2, end=' ')
+print(t1, t2, end=' ')
 for termo in range(3, quant_termo):
+    # Modelo de transição de valores.
     t3 = (t1 + t2)
+
     t1 = t2
     t2 = t3
 
