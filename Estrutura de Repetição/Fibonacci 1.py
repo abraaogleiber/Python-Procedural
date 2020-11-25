85cee6ed87e1bb4ee493afc9e6d66164b49e2a7c
Abraão Alves da Silva <abraaocursos2019@gmail.com>
2020-11-25 08:36:12 -0300

    Progroma_059
    Sequência de fibonacci.

---
+++ Estrutura de Repetição/Fibonacci 1.py
@@ -0,0 +1,23 @@
+"""
+Programa 059
+Área de estudos.
+data 25.11.2020     (Indefinida) Hs
+@Autor: Abraão A. Silva
+"""
+
+# Entrada da quantidade de termos.
+quant_termo = int(input('Quantos termos desejas mostrar.: ')) + 1
+
+# Definimos o primeiro e segundo termo.
+t1 = 1
+t2 = 1
+
+# Saída dos dados e geração do termo posterior, somando os dois anteriores.
+print(t1, end=' ')
+print(t2, end=' ')
+for termo in range(3, quant_termo):
+    t3 = (t1 + t2)
+    t1 = t2
+    t2 = t3
+
+    print(t3, end=' ')
