bbd0c0d6ac0af42893aa5d69ba3c369cd4b0e6ff
Abraão Alves da Silva <abraaocursos2019@gmail.com>
2020-11-25 13:59:45 -0300

    Programa_063 (Atualizado)
    Número primo. Essa atualização permite ao usuário ver os valores pelo qual o número foi divisível.

--- Estrutura de Repetição/Número primo.py
+++ Estrutura de Repetição/Número primo.py
@@ -17,4 +17,10 @@ for item in range(1, n+1):  # Iteração por item.
 if dividido == 2:
     print(f'O número {n} é Primo.')
 else:
-    print(f'O número {n} não é Primo.')
+    # Essa estrutura retorna na tela os valores pelo qual o número é divisível.
+    print(f'O número {n} não é Primo, pois é divisível por: ', end='')
+    for item in range(1, n+1):
+        if n % item == 0:
+            print(item, end=' ')
+
+print('\n')     # Formatação para facilitar minha visualização pelo terminal.
