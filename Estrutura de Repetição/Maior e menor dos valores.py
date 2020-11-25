7265050592f9a02df22857401886fd7a426693ee
Abraão Alves da Silva <abraaocursos2019@gmail.com>
2020-11-25 10:08:11 -0300

    Programa_062 (Atualizado)
    Atualização só permite valores entre o intervalo de 0 á 1000. Maior, menor e somatória de valores.

--- Estrutura de Repetição/Maior e menor dos valores.py
+++ Estrutura de Repetição/Maior e menor dos valores.py
@@ -5,30 +5,35 @@ data 25.11.2020     (Indefinida) Hs
 @Autor: Abraão A. Silva
 """
 
+print('Obs.: Não aceitamos valores menor que 0 e maiores que 1000.')
 n = int(input('1º Número.: '))
 
 contar = 2
 maior = menor = soma = n   # Apelidagem
 while 0 <= n <= 1000:
     n = int(input(f'{contar}º Número.: '))
-    soma += n   # Somatória dos valores.
+    if 0 <= n <= 1000:
+        soma += n   # Somatória dos valores.
 
-    # Condição que avalia quem é maior e menor.
-    if n > maior:
-        maior = n
-    if n < menor:
-        menor = n
+        # Condição que avalia quem é maior e menor.
+        if n > maior:
+            maior = n
+        if n < menor:
+            menor = n
 
-    continuar = str(input('Continuar(S/N)?.: ')).strip().upper()[0] 
-    # Repetição seguido de condição para avaliar a resposta do usuário.
-    while continuar not in 'SN':
-        print('Opção inválida.')
-        continuar = str(input('Continuar(S/N).: ')).strip().upper()[0]
-    if continuar == 'S':
-        contar += 1
-        continue    # Volta ao topo do "while" mais externo.
-    elif continuar == 'N':
-        break   # Para o programa.
+        continuar = str(input('Continuar(S/N)?.: ')).strip().upper()[0] 
+        # Repetição seguido de condição para avaliar a resposta do usuário.
+        while continuar not in 'SN':
+            print('Opção inválida.')
+            continuar = str(input('Continuar(S/N).: ')).strip().upper()[0]
+        if continuar == 'S':
+            contar += 1
+            continue    # Volta ao topo do "while" mais externo.
+        elif continuar == 'N':
+            break   # Para o programa.
+
+    else:
+        print('Não aceitamos valores menores que 0 e maiores que 1000.')
 
 print(f'O maior valor é {maior} e o menor {menor}')
 print(f'A soma dos valores é {soma}.')
