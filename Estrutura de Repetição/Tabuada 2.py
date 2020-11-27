88abcb2576bcb30b3d2366ff8275c79f8ee68dac
Abraão Alves da Silva <abraaocursos2019@gmail.com>
2020-11-27 07:12:44 -0300

    Programa_075 (Atualizado)
    Tabuada mais inteligente.

--- Estrutura de Repetição/Tabuada 2.py
+++ Estrutura de Repetição/Tabuada 2.py
@@ -11,5 +11,9 @@ inicio = int(input('Comerça em.: '))
 termina = int(input('Termina em.: '))
 
 # Gerador de tabuada.
-for t in range(inicio, termina+1):
-    print(f'{tabuada:2} + {t:2} = {tabuada + t:2}')
+if inicio < termina:
+    for t in range(inicio, termina+1):
+        print(f'{tabuada:2} + {t:2} = {tabuada + t:2}')
+
+else:
+    print('O inicio não poder igual ao termino e o termino nao pode ser maior que o inicio.')
