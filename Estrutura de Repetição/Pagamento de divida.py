25c61ab2076427e669abe1fd7c0468d603045344
Abraão Alves da Silva <abraaocursos2019@gmail.com>
2020-11-29 07:28:17 -0300

    Programa_080 (Precisa ser Refactorado)
    Tabela da divida.

---
+++ Estrutura de Repetição/Pagamento de divida.py
@@ -0,0 +1,52 @@
+"""
+Programa 080
+Área de estudos.
+data 29.11.2020     (Indefinida) Hs
+@Autor: Abraão A. Silva
+"""
+
+# Valor da divida.
+valor_divida = float(input('Valor da divida.: '))
+
+# Cabeçalho da tabela e montagem da tabela com "for"
+print('Valor da Divida(R$)  Parcelas    Juros(%)   valor da parcela(R$)')
+for parcela in range(0, 13, 3): # Os valores iterados representam as parcelas.
+
+    # Daqui pra baixo é feita uma série de verificações e calculos das parcelas e multas.
+    if parcela == 0:
+        parcela = 1
+        valor_parcela = valor_divida
+        juros = 0
+        print(f'{valor_divida:.2f}'.rjust(13)+f'{parcela}'.rjust(12)+f'{juros:.1f}'.rjust(13)+f'{valor_parcela:.2f}'.rjust(19)+'\n')
+
+    valor_parcela = float() # A cada laço "valor_parcela" é zerada.
+    if parcela == 3:
+        juros = (valor_divida * 10) / 100
+        for i in range(3):
+            valor_parcela = (valor_parcela + valor_divida + juros)
+        valor_parcela = (valor_parcela / 3)
+        print(f'{valor_divida:.2f}'.rjust(13)+f'{parcela}'.rjust(12)+f'{juros:.1f}'.rjust(15)+f'{valor_parcela:.2f}'.rjust(17)+'\n')
+
+    valor_parcela = float()
+    if parcela == 6:
+        juros = (valor_divida * 15) / 100
+        for i in range(6):
+            valor_parcela = (valor_parcela + valor_divida + juros)
+        valor_parcela = (valor_parcela / 6)
+        print(f'{valor_divida:.2f}'.rjust(13)+f'{parcela}'.rjust(12)+f'{juros:.1f}'.rjust(15)+f'{valor_parcela:.2f}'.rjust(17)+'\n')
+
+    valor_parcela = float()
+    if parcela == 9:
+        juros = (valor_divida * 20) / 100
+        for i in range(9):
+            valor_parcela = (valor_parcela + valor_divida + juros)
+        valor_parcela = (valor_parcela / 9)
+        print(f'{valor_divida:.2f}'.rjust(13)+f'{parcela}'.rjust(12)+f'{juros:.1f}'.rjust(15)+f'{valor_parcela:.2f}'.rjust(17)+'\n')
+
+    valor_parcela = float()
+    if parcela == 12:
+        juros = (valor_divida * 25) / 100
+        for i in range(12):
+            valor_parcela = (valor_parcela + valor_divida + juros)
+        valor_parcela = (valor_parcela / 12)
+        print(f'{valor_divida:.2f}'.rjust(13)+f'{parcela}'.rjust(12)+f'{juros:.1f}'.rjust(15)+f'{valor_parcela:.2f}'.rjust(17)+'\n')
