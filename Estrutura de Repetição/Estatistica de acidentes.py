c4f8ada8306412e3bd53deed0032d0d5ad5a40c8
Abraão Alves da Silva <abraaocursos2019@gmail.com>
2020-11-28 21:26:47 -0300

    Programa_079 (Atualizado)
    Estatística de acidentes.

--- Estrutura de Repetição/Estatistica de acidentes.py
+++ Estrutura de Repetição/Estatistica de acidentes.py
@@ -5,7 +5,7 @@ data 28.11.2020     (Indefinida) Hs
 @Autor: Abraão A. Silva
 """
 
-maior_indice, menor_indice, total_veiculos, total_acidentes, quant_cidade = 0, 999, 0, 0, 0
+maior_indice, menor_indice, total_veiculos, total_acidentes, soma_veiculos = 0, 999, 0, 0, 0
 for cidade in range(1, 6):
     print(f'{cidade}º Cidade...:::')
     num = int(input('Nº da Cidade.: '))
@@ -15,15 +15,15 @@ for cidade in range(1, 6):
     # Processamento
     if num_acidentes > maior_indice:
         maior_indice = num_acidentes
-        num_cidade = num
+        num_maior = num
 
     if num_acidentes < menor_indice:
         menor_indice = num_acidentes
-        num_cidade = num
+        num_menor = num
 
     # Media de acidentes na cidade com nº de veículos inferior á 2000.
     if quant_veiculos < 2000:
-        quant_cidade += 1
+        soma_veiculos += quant_veiculos
         total_acidentes += num_acidentes
 
     # Média de veículos.
@@ -31,9 +31,9 @@ for cidade in range(1, 6):
 
 
 media_veiculos = (total_veiculos / 5)
-media_acidentes = (total_acidentes / quant_cidade)
+media_acidentes = (total_acidentes / soma_veiculos)
 
-print(f'A cidade com maior número de acidentes tem Nº {num_cidade} com {maior_indice} acidentes registrados.')
-print(f'A cidade com menor número de acidentes tem Nº {num_cidade} com {menor_indice} acidentes registrados.')
-print(f'As cincos cidades juntas somam o total de {media_veiculos:.1f} veículos em circulação.')
-print(f'As cidades com nº de veiculos inferior á 2000, possue uma taxa de acidentes igual á {media_acidentes:.2f}.')
+print(f'Maior número de acidentes: Nº Cidade - {num_maior} Média - {maior_indice}')
+print(f'Menor número de acidentes: Nº Cidade - {num_menor} Média - {menor_indice}')
+print(f'As cidades tem em média {media_veiculos:.0f} veículos em circulação.')
+print(f'A média de acidentes nas cidades com número com de veículos inferior á 2000 é {media_acidentes:.2f}.')
