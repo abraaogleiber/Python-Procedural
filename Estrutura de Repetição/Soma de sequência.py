2d6580a7e205788cf8b279af2d9ab1371ab42afb
Abraão Alves da Silva <abraaocursos2019@gmail.com>
2020-12-01 13:22:25 -0300

    Programa_089 (Refactorado)
    Soma da sequência de valores.

--- Estrutura de Repetição/Soma de sequência.py
+++ Estrutura de Repetição/Soma de sequência.py
@@ -5,9 +5,15 @@ data 01.12.2020     (Indefinida) Hs
 @Autor: Abraão A. Silva
 """
 
-# Programa inacabado.
+# Coleta da quantidade de termos e inicialização de variáveis.
 n = int(input('Termos.: '))
 soma = 0
+divisor = 1
+
+# Iteramos a quantidade de termos que o usuário desejar.
 for operador in range(1, n+1):
-    for divisor in range(1, n+n, 2):
-            soma += (operador / divisor)
+    soma += (operador / divisor)    # Dividimos e logo somamos o resultado da divisão.
+
+    divisor += 2 # Incrementamos 1 ao divisor.
+
+print(soma)
