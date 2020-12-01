f57b253cb8b41a1972e025d119eb1fb24e9393ef
Abraão Alves da Silva <abraaocursos2019@gmail.com>
2020-12-01 19:47:01 -0300

    Programa_090
    Mostrando um vetor de cinco inteiros.

---
+++ Armazenamento: Listas/Mostrando um vetor 1.py
@@ -0,0 +1,12 @@
+"""
+Programa 090
+Área de estudos.
+data 01.12.2020     (Indefinida) Hs
+@Autor: Abraão A. Silva
+"""
+
+# Vetor com cinco inteiros.
+valores = [2, 3, 4, 5, 6]
+
+for valor in valores:   # Iteração por item.
+    print(valor, end=' ')
