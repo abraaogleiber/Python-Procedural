d0d11d535caa889610a498384c5d5c550feb0376
Abraão Alves da Silva <abraaocursos2019@gmail.com>
2020-12-02 07:22:37 -0300

    Programa_094
    Gerando lista de caracteres através de sua posição e vice-versa.

---
+++ Armazenamento: Listas/lista  de caracteres.py
@@ -0,0 +1,14 @@
+"""
+Programa 094
+Área de estudos.
+data 02.12.2020     (Indefinida) Hs
+@Autor: Abraão A. Silva
+"""
+
+# Com a função nativa da linguagem, podemos ver que caractere se refere a determinada posição na tabela.
+for n in range(33, 100):
+    print(chr(n))
+ 
+# Com esta função, conseguimos ver a exata posição de um caractere na tabela.
+print(ord('a'))
+
