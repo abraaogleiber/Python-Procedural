bb280801e22261e7c0574bb64e44847293a26fba
Abraão Alves da Silva <abraaocursos2019@gmail.com>
2020-12-02 07:39:18 -0300

    Programa_095 (Atualizado)
    Conta e mostra o número de consuantes em uma palavra.

--- Armazenamento: Listas/Conta consuantes.py
+++ Armazenamento: Listas/Conta consuantes.py
@@ -8,13 +8,19 @@ data 02.12.2020     (Indefinida) Hs
 # Recolhe a palavra digitada.
 palavra = str(input('Digite uma palavra.: ')).strip().upper()
 
-contar_consuantes = 0   # Variável contador
+lista_consuantes = list()   # Armazenará minhas consuantes.
 consuantes = 'BCDFGJKLMNPQRSTVWXZ' # String de caracteres.
 
 # Iteração por item
 for letra in palavra:
     if letra in consuantes: # Verifica se letra da palavra é pertinênte dentro de consuantes
-        contar_consuantes += 1
+        lista_consuantes.append(letra)
 
 # Saída
-print(f'Número de consunates na palavra.: {contar_consuantes}')
+print(f'Número de consunates na palavra.: {len(lista_consuantes)}')
+for consu in lista_consuantes:
+    print(consu, end=' ')
+
+# ou
+
+print('\n', lista_consuantes)
