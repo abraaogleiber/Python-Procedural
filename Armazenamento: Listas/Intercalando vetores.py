1ca810e9c7ad90506071d83a6e134c34ad2be51c
Abraão Alves da Silva <abraaocursos2019@gmail.com>
2020-12-03 08:38:26 -0300

    Programa_100 (Atualizado)
    Intercalando elementos de três listas.

--- Armazenamento: Listas/Intercalando vetores.py
+++ Armazenamento: Listas/Intercalando vetores.py
@@ -6,17 +6,19 @@ data 02.12.2020     (Indefinida) Hs
 """
 
 # Declaração e inicilização de listas.
-lista_um, lista_dois, lista_tudo = list(), list(), list()
+lista_um, lista_dois, lista_tres, lista_tudo = list(), list(), list(), list()
 
 print('Digite os 10(dez) primeiro dados.')
-for dado in range(1, 21):
+for dado in range(1, 31):
     dados = str(input(f'{dado}º Dado.: ')).strip().capitalize()
     if dado <= 10:
         lista_um.append(dados)  # Somente os 10 primeiro dados.
+    elif 10 < dado <= 20:
+        lista_dois.append(dados)    # Somente 10 dados da meio.
     else:
-        lista_dois.append(dados)    # Somente 10 ultimos dados.
+        lista_tres.append(dados)    # Somente 10 ultimos dados
 
 # Conjuto de todos os valores.
-lista_tudo = (lista_um + lista_dois)
+lista_tudo = (lista_um + lista_dois + lista_tres) # Concatenação.
 
 print(lista_tudo)
