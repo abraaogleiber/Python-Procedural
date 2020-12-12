6969ee4097586c677dda03fef1e52df3d3d1abbf
Abraão Alves da Silva <abraaocursos2019@gmail.com>
2020-12-12 08:50:10 -0300

    Programa_113
    Mecânismo que busca em um arquivo txt uma palavra e grava em outro arquivo txt.

---
+++ Arquivos/Avaliando receita.py
@@ -0,0 +1,19 @@
+"""
+Programa 113
+Área de estudos.
+data 12.12.2020     (Indefinida) Hs
+@Autor: Abraão A. Silva
+"""
+
+# Arquivo aberto para leitura.
+arquivo = open('/home/abraao/comparativo', 'r')
+
+# Arquivo criado e aberto para gravação subsequênte.
+busca = open('/home/abraao/retorno_busca.txt', 'a')     # Criamos um arquivo para armazenar as linhas
+for linha in arquivo:   # com a frase "departamento estadual de trânsito".
+    if 'DEPARTAMENTO ESTADUAL DE TRÂNSITO' in linha:
+        busca.write(linha)
+
+# Fim da manipulação.
+busca.close()
+arquivo.close()
