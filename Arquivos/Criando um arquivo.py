f6bd7b8a2c843b8d42ccb458a07918bbe56c6a0f
Abraão Alves da Silva <abraaocursos2019@gmail.com>
2020-12-13 12:22:54 -0300

    Programa_114
    Criando um arquivo aberto  .txt

---
+++ Arquivos/Criando um arquivo.py
@@ -0,0 +1,14 @@
+"""
+Programa 114
+Área de estudos.
+data 13.12.2020     (Indefinida) Hs
+@Autor: Abraão A. Silva
+"""
+
+# Criando um arquivo de texto vazio.
+arquivo = open('/home/abraao/Documentos/testando.txt', 'xt')
+arquivo.close()
+
+# Ou
+arquivo = open('/home/abraao/Documentos/Testando2.txt', 'w')    # ou 'wt'
+arquivo.close()
