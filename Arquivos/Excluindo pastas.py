c8181b5767531d04bf827be9e9b9fa9a1d30b512
Abraão Silva <66697369+abraaogleiber@users.noreply.github.com>
2020-12-13 15:32:14 -0300

    Programa_120
    Excluindo pastas (path) do servidor ou sistema, utilizando verificação antes de apaga-los.

--- Arquivos/Excluindo pastas.py
+++ Arquivos/Excluindo pastas.py
@@ -7,7 +7,7 @@ data 13.12.2020     (Indefinida) Hs
 
 import os
 
-# Vamos supor que queremos apagar um diretório inteiro.
+#  Vamos supor que queremos apagar um diretório inteiro.
 if os.path.exists('/home/abraao/meus_pornos'):  # Se dentro do meu servidor existe essa pasta.
     os.rmdir('/home/abraao/meus_pornos')
     print('Apagado..')
