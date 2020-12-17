4c437a757780ce4198e006c00d153ee5938509c0
Abraão Alves da Silva <abraaocursos2019@gmail.com>
2020-12-17 20:44:05 -0300

    Programa_128 (precisa ser Refactorado)
    Convertendo horário para padrão de 12 horas.

--- Funções e Procedimentos/Formato de 24 horas.py
+++ Funções e Procedimentos/Formato de 24 horas.py
@@ -6,35 +6,49 @@ data 16.12.2020     (Indefinida) Hs
 """ 
 
 
-def define_formato(hr, tr):
+def define_formato(hr, mn):
     "Docstring"   
 
-    f_pm = {13:1, 14:2, 15:3, 16:4, 17:5, 18:6, 19:7, 20:8, 21:9, 22:10, 23:11, 00:1}
+    f_pm = {13: 1, 14: 2, 15: 3, 16: 4, 17: 5, 18: 6, 19: 7, 20: 8, 21: 9, 22: 10, 23: 11, 00: 1}
 
-    if tr in 'AM':
-        f_hora = 'AM'
-        h_convertida = hr 
+    if hr in f_pm:
+        formato_hora = 'PM'
+        hora = f_pm[hr]
+        minuto = mn
 
-    elif tr in 'PM':
-        f_hora = 'PM'
-        h_convertida =  f_pm[hr]
-    
-    return (f_hora, h_convertida)
+    else:
+        formato_hora = 'AM'
+        hora = hr
+        minuto = mn    
+    return (hora, minuto, formato_hora)
 
 
-def saida_formato(hr, tr):
+def saida_formato(hr, mn, tr):
     "Docstring"
-    
-    print(f'{hr} {tr}')
+    if hr < 10:
+        if mn == 60:
+            print(f'{hr+1}:00 ({tr})')
+        else:
+            print(f'0{hr}:{mn} ({tr})')
+    else:
+        if mn == 60:
+            print(f'{hr+1}:00 ({tr})')
+        else: 
+            print(f'{hr}:{mn} ({tr})')
 
 
 def main():
     "A função principal, corpo."
 
     hora = int(input('Hora.: '))
-    turno = str(input('Turno(AM/PM).: ')).strip().upper()[:1]
+    minuto = int(input('Minuto.: '))
 
-    hora_formatada = define_formato(hr=hora, tr=turno)
-    saida_formato(hr=hora_formatada[1], tr=hora_formatada[0])
+    if hora > 24 or minuto > 60:
+        print('Formato de hora inválida.')
+        exit()
+
+    hora_formatada = define_formato(hr=hora, mn=minuto)
+    saida_formato(hr=hora_formatada[0], mn=hora_formatada[1], tr=hora_formatada[2])
+
 
 main()
