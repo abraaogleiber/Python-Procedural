ed8f0a96805e2933bc1b2640d413a0aaddf95d76
nick-secret <66697369+abraaogleiber@users.noreply.github.com>
2022-09-03 18:15:27 -0300

    Criando e renomeando variáveis, apagando comentários.

--- Estrutura Sequêncial/software do pintor (versão 2).py
+++ Estrutura Sequêncial/software do pintor (versão 2).py
@@ -1,29 +1,32 @@
 """
-Programa 016
-Área de estudos.
-data 17.11.2020     (Indefinida) Hs
+Programa 016 | Área de estudos. | data 17.11.2020 | (Indefinida) Hs
 @Autor: Abraão A. Silva
 """
 
-import math
+from math import ceil as aredondarCima
 
-area = float(input('Área.: '))
+capacidadeGalhao = 6
+capacidadeLata = 18
+areaPintadaPorLitro = 6
+valorLata = 80
+valorGalhao = 25
 
-# Sabendo que 1 litro de tinta pinta uma área de 6 metros quadrados, fizemos os calculos.
-quant_tinta = math.ceil(area / 6)
-quant_galhoes = math.ceil(quant_tinta / 3.6)
-quant_latas = math.ceil(quant_tinta / 18)
+area = float(input('Área -> '))
+
+# Sabendo que 1 litro de tinta pinta uma área de 6m².
+quantTinta = aredondarCima(area / areaPintadaPorLitro)
+quantGalhoes = aredondarCima(quantTinta / capacidadeGalhao)
+quantLatas = aredondarCima(quantTinta / capacidadeLata)
 
 # Calculo dos preços.
-preço_latas = (quant_latas * 80)
-preço_galhoes = (quant_galhoes * 25)
+precoLatas = (quantLatas * valorLata)
+precoGalhoes = (quantGalhoes * valorGalhao)
 
-# Saída do relatório dos valores.
 print(
      ' {:=^30}\n'.format(' Opções de Compra '),
-      f'Comprar somente Latas ------ {quant_latas}\n',
-                f'Valor R${preço_latas}\n',
-      f'Comprar somente Galhões ---- {quant_galhoes}\n',
-                f'Valor R${preço_galhoes}\n',
+      f'Comprar somente Latas ------ {quantLatas}\n',
+                f'Valor R${precoLatas}\n',
+      f'Comprar somente Galhões ---- {quantGalhoes}\n',
+                f'Valor R${precoGalhoes}\n',
      '{}\n'.format('='*30),
       )
