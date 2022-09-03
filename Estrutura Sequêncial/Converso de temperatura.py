b8f37cb8bbbefc3fd5a2908ef82a10efcf9d66f5
nick-secret <66697369+abraaogleiber@users.noreply.github.com>
2022-09-03 17:50:59 -0300

    Renomeando variáveis.

--- Estrutura Sequêncial/Converso de temperatura.py
+++ Estrutura Sequêncial/Converso de temperatura.py
@@ -1,18 +1,13 @@
 """
-Programa 009
-Área de estudos.
-data 15.11.2020     (Indefinida) Hs
+Programa 009  | Área de estudos. | data 15.11.2020 | (Indefinida) Hs
 @Autor: Abraão A. Silva
 """
 
-# Entrada de dados (input)
-temperatura_celsius = float(input('Temperatura(°C).: '))
-temperatura_fahrenheit = float(input('Temperatura(F).: '))
+tempCelsius = float(input('Temperatura(°C).: '))
+tempFahrenheit = float(input('Temperatura(F).: '))
 
-# Processamento das informações passadas.
-converte_para_celsius = (temperatura_fahrenheit - 32) * 5/9
-converte_para_fahrenheit = (temperatura_celsius * 9/5) + 32
+converteParaCelsius = (tempFahrenheit - 32) * (5 / 9)
+converteParaFahrenheit = (tempCelsius * 9/5) + 32
 
-# Saída do resultado da conversão.
-print(f'{temperatura_fahrenheit:.1f}(F) \t {converte_para_celsius:.1f}(°C) \n'
-      f'{temperatura_celsius:.1f}(°C) \t {converte_para_fahrenheit:.1f}(F)')
+print(f'{tempFahrenheit:.1f}(F) \t {converteParaCelsius:.1f}(°C) \n'
+      f'{tempCelsius:.1f}(°C) \t {converteParaFahrenheit:.1f}(F)')
