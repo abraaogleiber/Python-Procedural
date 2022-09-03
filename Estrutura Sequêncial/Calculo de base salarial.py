8f66990266fccaa14ec27e8e6a502ede35044cd5
nick-secret <66697369+abraaogleiber@users.noreply.github.com>
2022-09-03 17:18:45 -0300

    Apagando comentários desnecessários e renomeando variáveis.

--- Estrutura Sequêncial/Calculo de base salarial.py
+++ Estrutura Sequêncial/Calculo de base salarial.py
@@ -1,16 +1,11 @@
 """
-Programa 008
-Área de estudos.
-data 15.11.2020     (Indefinida) Hs
+Programa 008 | Área de estudos. | data 15.11.2020 | (Indefinida) Hs
 @Autor: Abraão A. Silva
 """
 
-# Capitação dos dados do usuário.
-ganho_hora = float(input('Valor da hora trabalhada.: '))
-horas_mes = int(input('Quantas horas trabalhadas(mês).: '))
+valorHora = float(input("Valor da hora trabalhada.: "))
+quantHorasMes = int(input("Quantas horas trabalhadas(mês).: "))
 
-# Calculo do salário base.
-ganho_mensal = (horas_mes * ganho_hora)
+salario = (quantHorasMes * valorHora)
 
-# Iteratividade com o usuário
-print(f'O seu salário base é de R${ganho_mensal}.')
+print(f'O seu salário base é de R${salario:.2f}')
