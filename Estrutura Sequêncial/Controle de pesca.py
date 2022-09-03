6489899cd951ef1d65b0cf3e95cd5a3a64ee5bac
nick-secret <66697369+abraaogleiber@users.noreply.github.com>
2022-09-03 17:46:13 -0300

    Atualizando variáveis e criando constante.

--- Estrutura Sequêncial/Controle de pesca.py
+++ Estrutura Sequêncial/Controle de pesca.py
@@ -1,24 +1,22 @@
 """
-Programa 013
-Área de estudos.
-data 16.11.2020     (Indefinida) Hs
+Programa 013 | Área de estudos. | data 16.11.2020 | (Indefinida) Hs
 @Autor: Abraão A. Silva
 """
 
-# Coleta do dia e peso pescado.
-dia = int(input('Dia da pesca.: '))
-peso = float(input('Quantidade de pescados(kg).: '))
+valorMultaPorQuilo = 4
 
-# O "excesso" tem o valor da quantidade pescada, menos o peso maximo permitido.
-excesso = (peso - 50)
+quantDia = int(input('Dia da pesca -> '))
+peso = float(input('Quantidade pescado(kg) -> '))
+
+# O "excesso" tem o valor da quantidade pescada(kg), menos o peso maximo permitido.
+pesoExcesso = (peso - 50)
 
 # "Multa" tem o valor do excesso, multiplicado pelo valor da multa por kg excedente.
-multa = excesso * 4
+valorMulta = (pesoExcesso * valorMultaPorQuilo)
 
-# Relatório da pesca.
-print('=-'*10, 'Dados da Pesca', '-='*10)
-print(f'Dia: {dia}')
+print('=-'*10, 'Relatório da Pesca', '-='*10)
+print(f'Dia: {quantDia}')
 print(f'Quantidade pescada(kg): {peso:.1f}kg')
-print(f'peso excedente(kg): {excesso:.1f}kg')
-print(f'Valor da multa: R${multa:.2f}')
+print(f'peso excedente(kg): {pesoExcesso:.1f}kg')
+print(f'Valor da multa: R${valorMulta:.2f}')
 print('='*30)
