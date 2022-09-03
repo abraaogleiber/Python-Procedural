2f528e6487350cac119bbb0e3f511cc1bc725e2a
nick-secret <66697369+abraaogleiber@users.noreply.github.com>
2022-09-03 17:12:37 -0300

    Refatorando nomes de variáveis.

--- Estrutura Sequêncial/Calculo da área de um quadrado.py
+++ Estrutura Sequêncial/Calculo da área de um quadrado.py
@@ -1,18 +1,15 @@
 """
-Programa 007
-Área de estudos.
-data 14.11.2020     (Indefinida) Hs
+Programa 007 | Área de estudos | data 14.11.2020 | (Indefinida) Hs
 @Autor: Abraão A. Silva
 """
 
 # Calculo da área de um quadrado.
 # Formula [A = l**2] ou [A = b.h]
 
-from math import floor  # Módulo matemático. 
+from math import floor as fr 
 
-# Calculo da área.
-lado = float(input('Um lado do quadrado.: '))
-area = lado ** 2    # Ou area = lado * lado
+lado = float(input("Valor de um lado do quadrado => "))
+areaQuadrado = (lado ** 2)
 
-print(f'A área do quadrado é igual a {round(area)}.')
-print('O dobro dessa área é {}'.format(floor(area*2)))
+print(f'A área do quadrado é igual a {round(areaQuadrado)}.')
+print('O dobro dessa área é {}.'.format(fr(areaQuadrado*2)))
