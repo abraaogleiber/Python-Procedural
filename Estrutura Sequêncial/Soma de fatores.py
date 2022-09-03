ac2eed92054d9ae5c98fbd93debcd8d7d6d0a1e8
nick-secret <66697369+abraaogleiber@users.noreply.github.com>
2022-09-03 18:20:29 -0300

    Modificando a estrutura do código.

--- Estrutura Sequêncial/Soma de fatores.py
+++ Estrutura Sequêncial/Soma de fatores.py
@@ -1,19 +1,17 @@
 """
-Programa 003
-Área de estudos.
-data 14.11.2020     (Indefinida) Hs
+Programa 003 | Área de estudos. | data 14.11.2020 | (Indefinida) Hs
 @Autor: Abraão A. Silva
 """
 
 # Modo 1
-num1 = int(input('Digite o primeiro valor.: '))
-num2 = int(input('Digite o segundo valor.: '))
+num1 = int(input('Digite o primeiro valor -> '))
+num2 = int(input('Digite o segundo valor -> '))
 
 soma = (num1 + num2)
 print('A soma dos fatores vale {}.'.format(soma))
 
 # Modo 2 
-num1 = int(input('Digite o primeiro valor.: '))
-num2 = int(input('Digite o segundo valor.: '))
+num1 = int(input('Digite o primeiro valor -> '))
+num2 = int(input('Digite o segundo valor -> '))
 
 print(f'A soma de {num1} e {num2} é igual á {num1+num2}.')
