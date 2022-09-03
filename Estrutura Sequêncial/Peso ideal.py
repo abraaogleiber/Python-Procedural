3b9e9f9df4f1b190749673bece5fb656db55aa28
nick-secret <66697369+abraaogleiber@users.noreply.github.com>
2022-09-03 18:06:45 -0300

    Renomeando variáveis e apagando comentários.

--- Estrutura Sequêncial/Peso ideal.py
+++ Estrutura Sequêncial/Peso ideal.py
@@ -1,16 +1,12 @@
 """
-Programa 011
-Área de estudos.
-data 16.11.2020     (Indefinida) Hs
+Programa 011 | Área de estudos. | data 16.11.2020 | (Indefinida) Hs
 @Autor: Abraão A. Silva
 """
 
-# Entrada de dados do usuário.
 print('='*5, 'Descubra seu peso ideal'.center(30), '='*5)
-altura_usuario = float(input('Digite sua altura.: '))
+alturaUsuario = float(input('Digite sua altura -> '))
 
 # Calculo para descubrir o peso ideal, baseado na altura.
-peso_ideal = (72.7 * altura_usuario) - 58
+pesoIdeal = (72.7 * alturaUsuario) - 58
 
-# saída de dados para o usuário.
-print(f'Seu peso ideal baseado na sua altura é de {peso_ideal:.1f}(kg).')
+print(f'Seu peso ideal baseado na sua altura é de {pesoIdeal:.1f}(kg).')
