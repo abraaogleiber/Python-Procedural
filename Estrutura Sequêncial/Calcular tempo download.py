afa02e1fe0f5ce74cc7920a087d0fea70981e44c
nick-secret <66697369+abraaogleiber@users.noreply.github.com>
2022-09-02 22:45:10 -0300

    Atualizando nomes das variáveis.

--- Estrutura Sequêncial/Calcular tempo download.py
+++ Estrutura Sequêncial/Calcular tempo download.py
@@ -12,19 +12,19 @@ Depois dividimos o tamanho do arquivo por pela taxa de transferência.
 Equação = tamanho do arquivo (megabytes) / (velocidade da trasmissão em megabits / 8(bits)) = tempo segundo.
 """
 
-tamanho_arquivo = float(input('Tamanho do arquivo(MB).: '))
-velocidade_rede = float(input('Velocidade da rede(Mb/s).: '))
+tamanhoArquivo = float(input('Tamanho do arquivo(MB).: '))
+velocidadeRede = float(input('Velocidade da rede(Mb/s).: '))
 
-# Calculo do tempo estimado de download em segundos e minutos.
-taxa_transferencia = (velocidade_rede / 8)
-tempo_segundo = tamanho_arquivo / (velocidade_rede / 8)
-tempo_minuto = (tempo_segundo / 60)
+# Calculo do tempo de download em segundos e minutos.
+taxaTransferencia = (velocidadeRede / 8)
+tempoEstimadoSegundo = tamanhoArquivo / (velocidadeRede / 8)
+tempoEstimadoMinuto = (tempoEstimadoSegundo / 60)
 
 # Relatório de Download.
 print(
     ' {:=^50}\n'.format(' Informações da Transferencia '),
-    f'Taxa de Transferência ---- {taxa_transferencia:.3f}(MB)\n',
-    f'Tempo de Download(s) ----- {tempo_segundo:.0f} segundo(s)\n',
-    f'Tempo de Download(m) ----- {tempo_minuto:.1f} minuto(s)\n',
+    f'Taxa de Transferência ---- {taxaTransferencia:.3f}(MB)\n',
+    f'Tempo de Download(s) ----- {tempoEstimadoSegundo:.0f} segundo(s)\n',
+    f'Tempo de Download(m) ----- {tempoEstimadoMinuto:.1f} minuto(s)\n',
     '{}\n'.format('='*50),   
      )
