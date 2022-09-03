3daedad24acf05595a9814f554edf7487718957b
nick-secret <66697369+abraaogleiber@users.noreply.github.com>
2022-09-03 17:30:50 -0300

    Eliminando comentários desnecessários.

--- Estrutura Sequêncial/Contra-check.py
+++ Estrutura Sequêncial/Contra-check.py
@@ -3,11 +3,10 @@ Programa 014 | Área de estudos. | data 17.11.2020 | (Indefinida) Hs
 @Autor: Abraão A. Silva
 """
 
-# Entrada dos dados do funcionário.
 ValorHora = float(input('Valor da sua hora.: '))
 quantHorasTrabalhadas = int(input('Quantidade de horas trabalhadas.: '))
 
-# Calculos de imp. Renda, inss, sindicato, salario líquido e bruto.
+# Cálculos e cobranças.
 salarioBruto = ValorHora * quantHorasTrabalhadas
 inss = (salarioBruto * 7.5) / 100
 sindicato = (salarioBruto * 5) / 100
@@ -15,7 +14,6 @@ totalDescontos = (inss + sindicato)
 salarioLiquido = (salarioBruto - totalDescontos)
 
 
-# Saída dos valores calculados.
 print('{:=^35}\n'.format(' Extrato Salarial '),
       '\n',
       f'Salário Bruto --------- [R${salario_bruto:.2f}]\n',
